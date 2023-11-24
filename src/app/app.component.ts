717696bdd6d25195af3208738001bd1f321c9cf7
Yannick Müller <mail@ynnk.dev>
2023-11-24 09:35:44 +0100

    add offline modus

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Network } from '@capacitor/network';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  public isConnected: boolean = true;
+
   constructor() {}
+
+  ngOnInit(): void {
+    Network.getStatus().then((status) => {
+      console.log('Network status', status);
+      this.isConnected = status.connected;
+    });
+    
+    Network.addListener('networkStatusChange', (status) => {
+      if (status.connected && !this.isConnected) {
+        window.location.reload();
+      }
+      this.isConnected = status.connected;
+
+    });
+
+  }
 }
--- src/app/chat/chat.page.ts
+++ src/app/chat/chat.page.ts
@@ -1,15 +1,9 @@
-import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Message } from '../model/message.model';
-import { Store } from '@ngrx/store';
-import { Subscription } from 'rxjs';
-import { MessageService } from '../service/message.service';
-import { RealtimeChannel, User } from '@supabase/supabase-js';
 import { UserService } from '../service/user.service';
 import { UserModel } from '../model/user.model';
 import { StorageService } from '../service/storage.service';
-import { BrodcastService } from '../service/brodcast.service';
-import { ChatService } from '../service/chat.service';
-import { HammerGestureConfig } from '@angular/platform-browser';
+import { Network } from '@capacitor/network';
 
 @Component({
   selector: 'app-chat',
@@ -34,7 +28,13 @@ export class ChatPage implements OnInit {
         this.currentUser = user;
         this.storageService.set('userId', user.id.toString());
       }
-      this.isLoading = false;
+
+      Network.getStatus().then((status) => {
+        console.log('Network status', status);
+        if (status.connected) {
+          this.isLoading = false;
+        }
+      });
     });
     this.storageService.set('username', username); 
   }
